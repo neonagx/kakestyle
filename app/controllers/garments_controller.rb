2ec17ef856ebf749a9b7c4d111912d2cd1b75550
Sukhyun Baik <neonagx@gmail.com>
2016-10-25 12:14:22 -0700

    completes controller in garments controller

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,15 +1,48 @@
 class GarmentsController < ApplicationController
+  before_action :set_garment, only: [:show, :edit, :update, :destroy]
   def index
     @garments = Garment.all
   end
 
   def show
-    @garment = Garment.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @garment = Garment.new
+  end
+
+  def create
+    @garment = Garment.new(garment_params)
+
+    if @garment.save
+      redirect_to @garment
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @garment.update_attributes(garment_params)
+      redirect_to @garment
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @garment.destroy
+    redirect_to garments_path
+  end
+  
+private
+  def garment_params
+    params.require(:garment).permit(:name, :color, :material, :price, :cool)
+  end
+
+  def set_garment
+    @garment = Garment.find(params[:id])
   end
 end
